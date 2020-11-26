c19caf9e43f0d7108f4f645c96c86fc085204cdc
cmr6689 <cmr6689@g.rit.edu>
2020-11-26 13:13:10 -0500

    added comments to view controller

--- route-runner/ViewController.swift
+++ route-runner/ViewController.swift
@@ -22,11 +22,15 @@ class ViewController: UIViewController, MGLMapViewDelegate {
 		
 		mapView = NavigationMapView(frame: view.bounds)
 		view.addSubview(mapView)
+		
+		//Set the map view delegate
 		mapView.delegate = self
 		
+		//Display user location on the map
 		mapView.showsUserLocation = true
 		mapView.setUserTrackingMode(.follow, animated: true, completionHandler: nil)
 		
+		//Gesture recognizer for long press added to map view
 		let longPress = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(_:)))
 		mapView.addGestureRecognizer(longPress)
 	}
@@ -34,10 +38,12 @@ class ViewController: UIViewController, MGLMapViewDelegate {
 	@objc func didLongPress(_ sender: UILongPressGestureRecognizer) {
 		guard sender.state == .began else { return }
 		
+		//Converts long press point to map coords
 		let point = sender.location(in: mapView)
 		let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
 		
 		if let origin = mapView.userLocation?.coordinate {
+			//Calculate the route from user location to destination
 			let home = origin
 			calculateRoute(from: origin, to: coordinate, to: home)
 		} else {
@@ -46,11 +52,20 @@ class ViewController: UIViewController, MGLMapViewDelegate {
 	}
 
 	func calculateRoute(from origin: CLLocationCoordinate2D, to point: CLLocationCoordinate2D, to home: CLLocationCoordinate2D) {
+		
+		//User location starting point
 		let origin = Waypoint(coordinate: origin, coordinateAccuracy: -1, name: "Start")
+		
+		//Point on map user long pressed
 		let point = Waypoint(coordinate: point, coordinateAccuracy: -1, name: "Point")
+		
+		//User location once again to return running route to home
 		let home = Waypoint(coordinate: home, coordinateAccuracy: -1, name: "Finish")
+		
+		//Specify the route is intended for walking/running
 		let routeOptions = NavigationRouteOptions(waypoints: [origin, point, home], profileIdentifier: .walking)
 		
+		//Generation of the route object
 		Directions.shared.calculate(routeOptions) { [weak self] (session, result) in
 			switch result {
 			case .failure(let error):
@@ -61,9 +76,14 @@ class ViewController: UIViewController, MGLMapViewDelegate {
 				}
 				strongSelf.route = route
 				strongSelf.routeOptions = routeOptions
+				
+				//Draw the route on the map
 				strongSelf.drawRoute(route: route)
+				
+				//Show the final destination waypoint
 				strongSelf.mapView.showWaypoints(on: route)
 				
+				//Display callout on destination annotation
 				if let annotation = strongSelf.mapView.annotations?.first as? MGLPointAnnotation {
 					annotation.title = "Start Navigation"
 					strongSelf.mapView.selectAnnotation(annotation, animated: true, completionHandler: nil)
@@ -76,25 +96,33 @@ class ViewController: UIViewController, MGLMapViewDelegate {
 		guard let routeShape = route.shape, routeShape.coordinates.count > 0 else {
 			return
 		}
+		//Convert the route coords into a polyline
 		var routeCoordinates = routeShape.coordinates
 		let polyline = MGLPolylineFeature(coordinates: &routeCoordinates, count: UInt(routeCoordinates.count))
 		
+		//Reset the route shape to new route if another route already on the map
 		if let source = mapView.style?.source(withIdentifier: "route-source") as? MGLShapeSource {
 			source.shape = polyline
 		} else {
 			let source = MGLShapeSource(identifier: "route-source", features: [polyline], options: nil)
+			
+			//Customize route line color and width
 			let lineStyle = MGLLineStyleLayer(identifier: "route-style", source: source)
 			lineStyle.lineColor = NSExpression(forConstantValue: #colorLiteral(red: 0.1897518039, green: 0.3010634184, blue: 0.7994888425, alpha: 1))
 			lineStyle.lineWidth = NSExpression(forConstantValue: 3)
+			
+			//Add source and style layer of the route to map view
 			mapView.style?.addSource(source)
 			mapView.style?.addLayer(lineStyle)
 		}
 	}
 	
+	//Method allows annotations to show callouts when tapped
 	func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
 		return true
 	}
 	
+	//Present the turn by turn navigation viewer when the callout is tapped
 	func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {
 		guard let route = route, let routeOptions = routeOptions else {
 			return
